b4a35217d83a98137e86c23db0fbc0dee8f3e9de
Leonardo <leonardoar2@al.insper.edu.br>
2022-11-25 19:09:24 -0300

    Erros de assets arrumados

--- assets.py
+++ assets.py
@@ -13,18 +13,18 @@ DESTROY_SOUND = 'destroy_sound'
 
 def load_assets():
     assets = {}
-    assets['background_img'] = pygame.image.load('assets/img/cenario.png').convert()
+    assets['background_img'] = pygame.image.load('PyGame/assets/img/cenario.png').convert()
     assets['background_img'] = pygame.transform.scale(assets['background_img'], (WIDTH, HEIGHT))
-    assets['police_img'] = pygame.image.load('assets/img/carro.png').convert_alpha()
+    assets['police_img'] = pygame.image.load('PyGame/assets/img/carro.png').convert_alpha()
     assets['police_img'] = pygame.transform.scale(assets['police_img'], (POLICE_WIDTH, POLICE_HEIGHT))
-    assets['mice_img'] = pygame.image.load('assets/img/rato.png').convert_alpha()
+    assets['mice_img'] = pygame.image.load('PyGame/assets/img/rato.png').convert_alpha()
     assets['mice_img'] = pygame.transform.scale(assets['mice_img'], (MICE_WIDTH, MICE_HEIGHT))
 
-    assets["score_font"] = pygame.font.Font('assets/font/PressStart2P.ttf', 28)
+    assets["score_font"] = pygame.font.Font('PyGame/assets/font/PressStart2P.ttf', 28)
     
     # Carrega os sons do jogo
-    pygame.mixer.music.load('assets/snd/tgfcoder-FrozenJam-SeamlessLoop.ogg')
+    pygame.mixer.music.load('PyGame/assets/snd/tgfcoder-FrozenJam-SeamlessLoop.ogg')
     pygame.mixer.music.set_volume(0.4)
-    assets['boom_sound'] = pygame.mixer.Sound('assets/snd/expl3.wav')
-    assets['destroy_sound'] = pygame.mixer.Sound('assets/snd/expl6.wav')
+    assets['boom_sound'] = pygame.mixer.Sound('PyGame/assets/snd/expl3.wav')
+    assets['destroy_sound'] = pygame.mixer.Sound('PyGame/assets/snd/expl6.wav')
     return assets
